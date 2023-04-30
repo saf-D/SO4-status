34b8713035f1115eb7ccb033e6ccb7948b1bc7f5
saf-D <117557845+saf-D@users.noreply.github.com>
2023-04-30 18:38:18 +0800

    FIXED

--- config.js
+++ config.js
@@ -6,11 +6,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm794262533-c50621a383c5044959f9d6d0',
     'm794262411-cbb0ef5b058283595330963a',
+    'm794262548-2623dfebfbaa01b202677c9a',
   ],
 
   // 日志天数
