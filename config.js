a4059c42d09d40d66b351345b6db43f288c3c437
saf-D <dkx11111@163.com>
2023-04-30 18:23:38 +0800

    ADD

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'SO4 Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm794262411-cbb0ef5b058283595330963a',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: '#'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/saf-D/SO4-status'
+    },
+  ],
+};
